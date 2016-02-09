d944bd5c6ad42dd9d229b7ea3dea55736fef4348
Cuzzbuzz <Wouter@192.168.0.221>
2016-02-09 18:08:12 +0100

    Added a short to appearance update (Future overrides) and fixed a small mistake in the same method.

--- src/org/maxgamer/rs/command/commands/Hide.java
+++ src/org/maxgamer/rs/command/commands/Hide.java
@@ -1,6 +1,7 @@
 package org.maxgamer.rs.command.commands;
 
 import org.maxgamer.rs.command.PlayerCommand;
+import org.maxgamer.rs.core.HeapDump;
 import org.maxgamer.rs.model.entity.mob.persona.player.Player;
 import org.maxgamer.rs.model.entity.mob.persona.player.Rights;
 
@@ -8,16 +9,18 @@ import org.maxgamer.rs.model.entity.mob.persona.player.Rights;
  * @author netherfoam
  */
 public class Hide implements PlayerCommand {
-
+	
 	@Override
 	public void execute(Player p, String[] args) {
 		if (p.isHidden()) {
 			p.show();
-		} else {
+		}
+		else {
 			p.hide();
 		}
+		HeapDump.dumpHeap("heap.bin", true);
 	}
-
+	
 	@Override
 	public int getRankRequired() {
 		return Rights.ADMIN;

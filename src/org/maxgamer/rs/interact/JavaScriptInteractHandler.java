757c7256f9b0bf2b9eb444b31f1bd221faa13328
Dirk Jamieson <dirk.jamieson@gmail.com>
2016-05-25 19:01:46 +1000

    + Add "wear()" function to Equipment + Fix for options not being found by JS sometimes.

--- src/org/maxgamer/rs/interact/JavaScriptInteractHandler.java
+++ src/org/maxgamer/rs/interact/JavaScriptInteractHandler.java
@@ -2,6 +2,8 @@ package org.maxgamer.rs.interact;
 
 import java.io.File;
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Arrays;
 
 import org.maxgamer.rs.lib.log.Log;
 import org.maxgamer.rs.model.action.Action;
@@ -26,7 +28,7 @@ public class JavaScriptInteractHandler implements InteractionHandler{
 	 * @param option the option that was clicked
 	 * @return the file or null if no good file was found
 	 */
-	private File get(Interactable entity, String option){
+	private ArrayList<File> get(Interactable entity, String option){
 		Class<?> clazz = entity.getClass();
 		
 		String entityName = entity.getName();
@@ -35,24 +37,26 @@ public class JavaScriptInteractHandler implements InteractionHandler{
 			entityName = entityName.replaceAll("[^A-Za-z0-9\\-_ ].*", "").trim();
 		}
 		
+		ArrayList<File> files = new ArrayList<File>();
+		
 		// We exhaust all superclass options as well as the base class
 		while(clazz != Object.class){
 			String className = clazz.getSimpleName().toLowerCase();
 			
 			File f = new File(INTERACTION_FOLDER + File.separator + className, entityName + ".js");
 			if(f.exists()){
-				return f;
+				files.add(f);
 			}
 			
 			f = new File(INTERACTION_FOLDER + File.separator + className , option + ".js");
 			if(f.exists()){
-				return f;
+				files.add(f);
 			}
 			
 			clazz = clazz.getSuperclass();
 		}
 		
-		return null;
+		return files;
 	}
 	
 	/**
@@ -94,11 +98,7 @@ public class JavaScriptInteractHandler implements InteractionHandler{
 	 */
 	@Interact
 	public void javascript(Mob source, Interactable target, String option) throws SuspendExecution, NotHandledException {
-		File f = this.get(target, option);
-		if(f == null) {
-			throw new NotHandledException();
-		}
-		
+		ArrayList<File> files = this.get(target, option);
 		String function = this.toFunction(option);
 		
 		if(target instanceof Mob){
@@ -106,42 +106,50 @@ public class JavaScriptInteractHandler implements InteractionHandler{
 			((Mob) target).setFacing(Facing.face(source.getCenter()));
 		}
 		
-		JavaScriptFiber fiber = new JavaScriptFiber();
-		
-		try{
-			fiber.set("fiber", fiber);
-			fiber.set("player", source);
+		for(File f : files) {
+			JavaScriptFiber fiber = new JavaScriptFiber();
 			
-			if(fiber.parse("lib/core.js").isFinished() == false){
-				throw new RuntimeException("lib/core.js cannot contain pauses outside of functions."); 
+			try{
+				fiber.set("fiber", fiber);
+				fiber.set("player", source);
+				
+				if(fiber.parse("lib/core.js").isFinished() == false){
+					throw new RuntimeException("lib/core.js cannot contain pauses outside of functions."); 
+				}
+				if(fiber.parse("lib/dialogue.js").isFinished() == false){
+					throw new RuntimeException("lib/dialogue.js cannot contain pauses outside of functions."); 
+				}
+				if(fiber.parse(f).isFinished() == false){
+					throw new RuntimeException(f + " cannot contain pauses outside of functions.");
+				}
 			}
-			if(fiber.parse("lib/dialogue.js").isFinished() == false){
-				throw new RuntimeException("lib/dialogue.js cannot contain pauses outside of functions."); 
+			catch(IOException e){
+				e.printStackTrace();
 			}
-			if(fiber.parse(f).isFinished() == false){
-				throw new RuntimeException(f + " cannot contain pauses outside of functions.");
+			
+			if(target instanceof Entity){
+				source.face((Entity) target);
 			}
-		}
-		catch(IOException e){
-			e.printStackTrace();
-		}
-		
-		if(target instanceof Entity){
-			source.face((Entity) target);
-		}
-		
-		JavaScriptCall call = null;
-		try {
-			call = fiber.invoke(function, source, target);
-		}
-		catch (NoSuchMethodException e) {
-			Log.debug("File " + f + " exists, but the function " + function + "() does not.");
-			throw new NotHandledException();
-		}
-		
-		Action.wait(1);
-		while(call.isFinished() == false){
+			
+			JavaScriptCall call = null;
+			try {
+				call = fiber.invoke(function, source, target);
+			}
+			catch (NoSuchMethodException e) {
+				continue;
+			}
+			
 			Action.wait(1);
+			while(call.isFinished() == false){
+				Action.wait(1);
+			}
+			return;
 		}
+		
+		if(files.isEmpty() == false) {
+			Log.debug("Files " + Arrays.toString(files.toArray(new File[files.size()])) + " exists, but the function " + function + "() does not.");
+		}
+		
+		throw new NotHandledException();
 	}
 }
--- src/org/maxgamer/rs/model/item/inventory/Equipment.java
+++ src/org/maxgamer/rs/model/item/inventory/Equipment.java
@@ -8,7 +8,9 @@ import java.util.List;
 
 import org.maxgamer.rs.lib.log.Log;
 import org.maxgamer.rs.model.entity.mob.Bonuses;
+import org.maxgamer.rs.model.entity.mob.InventoryHolder;
 import org.maxgamer.rs.model.entity.mob.Mob;
+import org.maxgamer.rs.model.events.mob.MobEquipEvent;
 import org.maxgamer.rs.model.item.EquipmentSet;
 import org.maxgamer.rs.model.item.ItemStack;
 import org.maxgamer.rs.model.item.WieldType;
@@ -211,5 +213,85 @@ public class Equipment extends Container {
 
 	public ItemStack getGloves() {
 		return get(WieldType.GLOVES);
+	}
+	
+	/**
+	 * Attempts to equip the given item
+	 * @param item the item to equip from inventory (if applicable)
+	 * @param inventorySlot the slot, may be -1 for any
+	 * @throws ContainerException if the equip could not be performed
+	 */
+	public void wear(ItemStack item, int inventorySlot) throws ContainerException {
+		Container equip = this;
+		Container inv = null;
+		if (getOwner() instanceof InventoryHolder) {
+			inv = ((InventoryHolder) getOwner()).getInventory();
+			if(inventorySlot == -1 || item.matches(inv.get(inventorySlot)) == false){
+				inventorySlot = inv.getSlotOf(item);
+			}
+		}
+
+		WieldType target = item.getWeapon().getSlot();
+		ItemStack old = equip.get(target.getSlot());
+
+		if (old != null) {
+			if (old.matches(item)) {
+				if (old.getAmount() < old.getStackSize()) {
+					// Add the stacks together
+
+					if (old.getAmount() + item.getAmount() > old.getStackSize()) {
+						// Not all of the items can be added to the slot, but
+						// some can->
+						long swap = old.getStackSize() - old.getAmount();
+						old = old.setAmount(old.getStackSize());
+						item = item.setAmount(item.getAmount() - swap);
+
+						if (inv != null) {
+							inv.set(inventorySlot, item);
+						}
+						equip.set(target.getSlot(), old);
+					} else {
+						// All of the items can be added to the slot->
+						inv.set(inventorySlot, null);
+						equip.set(target.getSlot(), old.setAmount(old.getAmount() + item.getAmount()));
+					}
+				} else {
+					// We're already at the max stack size->
+					// Nothing will be accomplished by equipping this->
+					return;
+				}
+			} else {
+				// The two items do not match-> Remove the old one, equip the
+				// new one->
+				MobEquipEvent event = new MobEquipEvent(getOwner(), old, item);
+				event.call();
+				if (event.isCancelled()){
+					return;
+				}
+				inv.set(inventorySlot, old);
+				equip.set(target.getSlot(), item);
+			}
+		} else {
+			MobEquipEvent event = new MobEquipEvent(getOwner(), old, item);
+			event.call();
+			if (event.isCancelled()){
+				return;
+			}
+			// There is currently no other item equipped in the slot->
+			inv.set(inventorySlot, null);
+			
+			equip.set(target.getSlot(), item);
+		}
+
+		ItemStack weapon = equip.get(WieldType.WEAPON.getSlot());
+		ItemStack gloves = equip.get(WieldType.GLOVES.getSlot());
+		if (weapon != null) {
+			getOwner().getModel().setRenderAnimationId(weapon.getDefinition().getRenderAnim());
+		} else if (gloves != null) {
+			getOwner().getModel().setRenderAnimationId(gloves.getDefinition().getRenderAnim());
+		} else {
+			getOwner().getModel().setRenderAnimationId(1426);
+		}
+		return;
 	}
 }

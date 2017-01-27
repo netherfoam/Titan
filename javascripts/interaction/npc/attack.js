458b411babd5cf04f99c1a2a6bc03b3cc204a39c
Dirk Jamieson <dirk.jamieson@gmail.com>
2017-01-27 19:12:44 +1000

    remove unused js code and make attacks more responsive

--- javascripts/interaction/npc/attack.js
+++ javascripts/interaction/npc/attack.js
@@ -1,6 +1,13 @@
 module.exports = {
     attack: function(player, npc){
+        // No changes
+        if(player.getTarget() == npc) return;
+
     	player.setTarget(npc);
+
+    	// We immediately tick the player's action queue, because changing
+    	// target doesn't cost you a turn.
+    	player.getActions().tick();
     }
 }
 

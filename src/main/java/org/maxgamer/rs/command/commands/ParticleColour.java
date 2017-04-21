d078cf669346865078c31378d9747a6962c066ce
Dirk Jamieson <dirk.jamieson@gmail.com>
2017-04-21 18:48:58 +1000

    particle cleanup

--- src/main/java/org/maxgamer/rs/command/commands/ParticleColour.java
+++ src/main/java/org/maxgamer/rs/command/commands/ParticleColour.java
@@ -1,34 +1,33 @@
 package org.maxgamer.rs.command.commands;
 
 import org.maxgamer.rs.command.PlayerCommand;
+import org.maxgamer.rs.model.entity.mob.MobParticles;
 import org.maxgamer.rs.model.entity.mob.persona.player.Player;
 import org.maxgamer.rs.model.entity.mob.persona.player.Rights;
-
-import java.util.Random;
+import org.maxgamer.rs.util.Erratic;
 
 public class ParticleColour implements PlayerCommand {
 
     @Override
     public void execute(Player p, String[] args) {
         if (args[0].equalsIgnoreCase("print")) {
-            p.sendMessage("Colour: " + Integer.toHexString(p.getModel().red) + "" + Integer.toHexString(p.getModel().green) + "" + Integer.toHexString(p.getModel().blue));
-            p.sendMessage("Ambient: " + p.getModel().ambient);
-            p.sendMessage("Intensity: " + p.getModel().intensity);
+            MobParticles particles = p.getModel().getParticles();
+            if(particles == null) {
+                p.sendMessage("No particles active");
+                return;
+            }
+
+            p.sendMessage("Colour: " + Integer.toHexString(particles.getRed()) + "" + Integer.toHexString(particles.getGreen()) + "" + Integer.toHexString(particles.getBlue()));
+            p.sendMessage("Ambient: " + particles.getAmbient());
+            p.sendMessage("Intensity: " + particles.getIntensity());
             return;
         } else if (args[0].equalsIgnoreCase("default")) {
-            p.getModel().applyCustom = false;
-            p.getModel().setChanged(true);
+            p.getModel().setParticles(null);
             p.sendMessage("Default sweet default!");
             return;
         } else if (args[0].equalsIgnoreCase("random")) {
-            Random r = new Random();
-            p.getModel().red = r.nextInt(255);
-            p.getModel().green = r.nextInt(255);
-            p.getModel().blue = r.nextInt(255);
-            p.getModel().ambient = 30 + r.nextInt(30);
-            p.getModel().intensity = 30 + r.nextInt(30);
-            p.getModel().applyCustom = true;
-            p.getModel().setChanged(true);
+            MobParticles particles = new MobParticles(Erratic.nextInt(255), Erratic.nextInt(255), Erratic.nextInt(255), Erratic.nextInt(30, 60), Erratic.nextInt(30, 60));
+            p.getModel().setParticles(particles);
             p.sendMessage("Ahhh... Nothing better than a random colour!");
             return;
         }
@@ -41,13 +40,14 @@ public class ParticleColour implements PlayerCommand {
         String red = "" + args[0].substring(0, 2);
         String green = "" + args[0].substring(2, 4);
         String blue = "" + args[0].substring(4, 6);
-        p.getModel().red = Integer.parseInt(red, 16);
-        p.getModel().green = Integer.parseInt(green, 16);
-        p.getModel().blue = Integer.parseInt(blue, 16);
-        p.getModel().ambient = Integer.parseInt(args[1]);
-        p.getModel().intensity = Integer.parseInt(args[2]);
-        p.getModel().applyCustom = true;
-        p.getModel().setChanged(true);
+        int r = Integer.parseInt(red, 16);
+        int g = Integer.parseInt(green, 16);
+        int b = Integer.parseInt(blue, 16);
+        int a = Integer.parseInt(args[1]);
+        int i = Integer.parseInt(args[2]);
+
+        MobParticles particles = new MobParticles(r, g, b, a, i);
+        p.getModel().setParticles(particles);
     }
 
     @Override
--- src/main/java/org/maxgamer/rs/model/entity/mob/MobModel.java
+++ src/main/java/org/maxgamer/rs/model/entity/mob/MobModel.java
@@ -14,45 +14,77 @@ import java.io.IOException;
  * @author netherfoam
  */
 public abstract class MobModel implements YMLSerializable {
-    public int red, green, blue, ambient, intensity;
-    public boolean applyCustom;
+    /**
+     * Fetches the maximum colour number for the given slot, where the given
+     * slot is a value from 0-4 representing the piece of character design that
+     * is being modified. Values which are out of bounds will likely cause the
+     * character to become partially invisible. This method is for setting the
+     * colour of things such as beards, skin, torso and legs
+     *
+     * @param index the id of the look to edit (eg beard, torso, bottoms, chest)
+     *              0-4
+     * @return the maximum value (Eg, valid values are 0-maximum inclusive)
+     */
+    public static int getMaxColor(int index) {
+        switch (index) {
+            case 0:
+                return 25;
+            case 1:
+                return 29;
+            case 2: // bottom
+                return 29;
+            case 3:
+                return 5;
+            case 4:
+                return 5;
+        }
+
+        throw new IllegalArgumentException();
+    }
+
     /**
      * True if this model has been modified and needs to be resent to all
      * interested players
      */
     private boolean changed = true;
+
     /**
      * True if this model is female, false if it is male
      */
     private boolean female = false;
+
     /**
      * True if this model is to display PKing skull, false if not
      */
     private boolean skulled = false;
+
     /**
      * The prayer icon this model is to display, -1 for none
      */
     private byte prayerIcon = -1;
+
     /**
      * True if this mob is to display the combat colour - eg. Green for lower
      * level, red for higher level
      */
     private boolean combatColour = true;
+
     /**
      * The name of this mob, I don't believe this works for NPC's but it is sent
      * for both anyway
      */
     private String name = "Unknown";
+
     /**
      * The combat level for this mob, I don't believe this works for NPC's but
      * it is sent for both anyway This is an unsigned byte.
      */
     private byte combatLevel = 3;
+
     /**
      * The title ID for this mob, this is retrieved from the cache
      */
     private int title = 0;
-
     /*
      * Taken from
      * http://www.rune-server.org/runescape-development/rs-503-client-
@@ -82,10 +114,14 @@ public abstract class MobModel implements YMLSerializable {
      * skin color: gets darker the bigger the number
      */
     private int renderAnimationId = 1426;
+
     /**
      * The colours for a player
      */
     private byte[] colour = new byte[]{3, 16, 16, 11, 14,};
+
+    private MobParticles particles;
+
     /**
      * A cached version of this mobs model in byte form. If the changed flag is
      * set, this data will be scrapped and regenerated. Otherwise, for example,
@@ -93,34 +129,6 @@ public abstract class MobModel implements YMLSerializable {
      * receive this cached data as an update instead.
      */
     private byte[] cache;
-
-    /**
-     * Fetches the maximum colour number for the given slot, where the given
-     * slot is a value from 0-4 representing the piece of character design that
-     * is being modified. Values which are out of bounds will likely cause the
-     * character to become partially invisible. This method is for setting the
-     * colour of things such as beards, skin, torso and legs
-     *
-     * @param index the id of the look to edit (eg beard, torso, bottoms, chest)
-     *              0-4
-     * @return the maximum value (Eg, valid values are 0-maximum inclusive)
-     */
-    public static int getMaxColor(int index) {
-        switch (index) {
-            case 0:
-                return 25;
-            case 1:
-                return 29;
-            case 2: // bottom
-                return 29;
-            case 3:
-                return 5;
-            case 4:
-                return 5;
-        }
-
-        throw new IllegalArgumentException();
-    }
 
     /**
      * Returns the cached update data
@@ -153,15 +161,6 @@ public abstract class MobModel implements YMLSerializable {
 
                 appendUpdate(out);
 
-                if (applyCustom) {
-                    out.writeByte(1);
-                    out.writeByte(red);
-                    out.writeByte(green);
-                    out.writeByte(blue);
-                    out.writeByte(intensity);
-                    out.writeByte(ambient);
-                } else out.writeByte(0);
-
                 byte[] colour = getColour();
                 for (int i = 0; i < 5; i++) {
                     out.writeByte(colour[i]);
@@ -181,10 +180,8 @@ public abstract class MobModel implements YMLSerializable {
                     out.writeByte(0); // Boosted
                     out.writeByte(0); // Combat range (-1 for none)
                 } else {
-                    out.writeShort(0);
+                    out.writeShort(0); // Skill rating
                 }
-                // }else{
-                // out.writeShort(?); // skillRating
 
                 // No clue
                 out.writeByte(0);
@@ -193,6 +190,17 @@ public abstract class MobModel implements YMLSerializable {
                 // bytes we've just written. If we were to write extra bytes here, the client would just
                 // discard them harmlessly. So, here would be the perfect place to eg. Re-add particle
                 // effects!
+
+                if (particles != null) {
+                    out.writeByte(1);
+                    out.writeByte(particles.getRed());
+                    out.writeByte(particles.getGreen());
+                    out.writeByte(particles.getBlue());
+                    out.writeByte(particles.getIntensity());
+                    out.writeByte(particles.getAmbient());
+                } else {
+                    out.writeByte(0);
+                }
 
                 this.cache = data.toByteArray();
             } catch (IOException e) {
@@ -367,6 +375,16 @@ public abstract class MobModel implements YMLSerializable {
     }
 
     /**
+     * Sets the particle effect to render on this player. This is a client side effect only available in the custom client.
+     *
+     * @param particles the particles to set, may be null
+     */
+    public void setParticles(MobParticles particles) {
+        this.particles = particles;
+        this.setChanged(true);
+    }
+
+    /**
      * Sets the changed value. If this is set to true, on the next mask update,
      * this model will be sent to all nearby players
      *
@@ -398,12 +416,11 @@ public abstract class MobModel implements YMLSerializable {
         map.set("female", this.female);
         map.set("level", this.combatLevel & 0xFF); // Save as unsigned.
         map.set("title", this.title);
-        map.set("red", this.red);
-        map.set("green", this.green);
-        map.set("blue", this.blue);
-        map.set("ambient", this.ambient);
-        map.set("intensity", this.intensity);
-        map.set("apply", this.applyCustom);
+
+        if(particles != null) {
+            map.set("particles", particles);
+        }
+
         return map;
     }
 
@@ -413,12 +430,10 @@ public abstract class MobModel implements YMLSerializable {
         this.female = map.getBoolean("female", this.female);
         this.combatLevel = (byte) map.getInt("combatLevel", this.combatLevel);
         this.title = map.getInt("title", this.title);
-        this.red = map.getInt("red", this.red);
-        this.green = map.getInt("green", this.green);
-        this.blue = map.getInt("blue", this.blue);
-        this.ambient = map.getInt("ambient", this.ambient);
-        this.intensity = map.getInt("intensity", this.intensity);
-        this.applyCustom = map.getBoolean("apply", this.applyCustom);
+
+        if(map.containsKey("particles")) {
+            this.particles = new MobParticles(map.getSection("particles"));
+        }
     }
 
     public int getRenderAnimationId() {
@@ -427,5 +442,9 @@ public abstract class MobModel implements YMLSerializable {
 
     public void setRenderAnimationId(int renderAnimationId) {
         this.renderAnimationId = renderAnimationId;
+    }
+
+    public MobParticles getParticles() {
+        return particles;
     }
 }
